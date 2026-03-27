4811b0e1c8e1a41873685de89bc64b1136437bb1
prasanna757 <prasannadpatil47@gmail.com>
2026-03-27 09:25:03 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name ='medical-chatbot',
+    version = '0.1.0',
+    author='Prasana',
+    author_email='prasannadpatil47@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+
+)
